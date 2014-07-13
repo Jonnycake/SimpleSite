e97f48ea5b84c46de4a0614aad95f074a57bf1a0
Jon Stockton <jonstockton1416@gmail.com>
2014-07-13 11:50:47 -0400

    Cleaning branch of new files.

--- js/class2tag.js
+++
@@ -1,9 +0,0 @@
-function class2tag(tagname,classname)
-{
-	var elements=document.getElementsByTagName(tagname);
-	for(var x=0;x<elements.length;x++)
-	{
-		var element=elements[x];
-		element.setAttribute("class",element.getAttribute("class")+" "+classname);
-	}
-}
